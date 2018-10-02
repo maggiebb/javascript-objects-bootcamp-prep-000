3eacc97663c6f5fbbad353f170d4485dd4c2b7da
Margaret Brett-Bowen <maggie.erickson@gmail.com>
2018-10-02 20:51:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {Beatles:"Penny Lane"};
 function updatePlaylist (playlist, artistName, songTitle){
-  return Object.assign({},playlist,{artistName:songTitle})
+  return Object.assign({},playlist,{"artistName":songTitle})
 }
 
